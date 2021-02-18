cd38c6a4199b9dcf8d2978448cbe05f4eb40b620
Joseph Perez <perezj0230@gmail.com>
2021-02-18 16:48:43 -0500

    added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-    return (
-        <>
-            <h1>hello</h1>
-            <body className="table">hi</body>
-        </>
-    );
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Vector Calculations</h1>
+    </div>
+  );
+};
+
+export default Header;
